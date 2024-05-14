47dee122b701ce9203b29db487549f2c11150d1c
Ouazane <mohamedouazane@yahoo.com>
2024-05-14 05:30:13 -0700

    2-app.py

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ Basic Flask app """
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_babel import Babel
 
 
@@ -17,10 +17,16 @@ app.config.from_object(Config)
 babel = Babel(app)
 
 
-@app.route('/', )
+@babel.localeselector
+def get_locale():
+    """ determine the best match with our supported languages"""
+    return request.accept_languages.best_match(Config.LANGUAGES)
+
+
+@app.route('/')
 def index():
-    """ First Route """
-    return render_template('0-index.html')
+    """ First Route"""
+    return render_template('2-index.html')
 
 
 if __name__ == '__main__':
