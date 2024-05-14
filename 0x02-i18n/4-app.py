1274a300c0e683edd23b304feea97e25f4c74657
Meed <mohamedouazane@yahoo.com>
2024-05-14 16:24:22 +0000

    4-app.py

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -17,6 +17,7 @@ app.config.from_object(Config)
 babel = Babel(app)
 
 
+@babel.localeselector
 def get_locale():
     """ determine the best match with our supported languages"""
 
@@ -24,9 +25,6 @@ def get_locale():
     if locale in Config.LANGUAGES:
         return locale
     return request.accept_languages.best_match(Config.LANGUAGES)
-
-
-babel.init_app(app, locale_selector=get_locale)
 
 
 @app.route('/')
