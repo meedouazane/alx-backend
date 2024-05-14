7fd2d20980b111eb6a520980534cfeab9d19374e
Meed <mohamedouazane@yahoo.com>
2024-05-14 15:50:53 +0000

    4-app.py

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+""" Basic Flask app """
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """ Class Config """
+
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+def get_locale():
+    """ determine the best match with our supported languages"""
+
+    locale = request.args.get('locale')
+    if locale in Config.LANGUAGES:
+        return locale
+    return request.accept_languages.best_match(Config.LANGUAGES)
+
+
+@app.route('/')
+def index():
+    """ First Route"""
+    return render_template('4-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
