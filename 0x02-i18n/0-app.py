23c3af66595acbe3f907dbc9e3a54df9392c4dde
Ouazane <mohamedouazane@yahoo.com>
2024-05-14 04:21:59 -0700

    0-app.py

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -7,7 +7,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    """ first route """
+    return render_template('1-index.html')
 
 
 if __name__ == '__main__':
