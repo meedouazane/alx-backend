da48c299f3a8830e8b312865cd4b2e152d51fbfb
Meed <mohamedouazane@yahoo.com>
2024-05-14 22:18:30 +0000

    add annotation

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -18,7 +18,7 @@ babel = Babel(app)
 
 
 @babel.localeselector
-def get_locale():
+def get_locale() -> str:
     """ determine the best match with our supported languages"""
     return request.accept_languages.best_match(Config.LANGUAGES)
 
