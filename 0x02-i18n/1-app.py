03bba89a80f44d50612143e1e8eab6868767938a
Ouazane <mohamedouazane@yahoo.com>
2024-05-14 04:42:09 -0700

    1-app.py

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+""" Basic Flask app """
+from datetime import UTC
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+
+
+class Config:
+    """ Class Config """
+
+    LANGUAGES = ["en", "fr"]
+
+
+bable = Babel(app, locale_selector=Config.LANGUAGES, timezone_selector=UTC)
+
+
+@app.route('/')
+def index():
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
